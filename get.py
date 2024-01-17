84be87f3fa81a76ffd7bde4623811f5266ba253d
pc <1156602859@qq.com>
2024-01-17 16:30:43 +0800

    turbo performance

---
+++ get.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 15:39:36 2023
+
+@author: Lenovo
+"""
+'''
+try:
+    print("right")
+    x=5
+    y=0
+    b = x/y
+    print(b)
+except Exception as e:
+    print("error")
+    print(e)
+'''
+a = {1:'a',2:'b'}
+b= {3:'c'}
+a='ss'
+b='ff'
+print(a+b)
+c = {1:'a',2:'b'}
+print(c.pop(1))
